70d85ded349a8107f50c0f436e7b26944110ca1a
keiya01 <keiya.s.0210@gmail.com>
2020-02-27 11:00:31 +0900

    Update index.js

--- tree_shaking/src/index.js
+++ tree_shaking/src/index.js
@@ -1,13 +1,34 @@
-import { cube } from './math'; // square()をimportしていない
+import { map } from 'lodash';
+// import { cube } from './math'; // square()をimportしていない
 
-function component() {
-  const elm = document.createElement('pre');
-  elm.innerHTML = [
-    'Hello Webpack',
-    '5 cubed = ' + cube(5)
-  ].join('\n\n');
+/**
+ * このように書いた場合、moduleを書き換える事ができるため、webpackはTree Shakingを行えない
+ * そのため、別ファイルで`export { cube } from './math'`のようにして、自分で削らなければいけない
+ * import('./math').then(module => {
+ *   function component() {
+ *     const elm = document.createElement('pre');
+ *     elm.innerHTML = [
+ *       'Hello Webpack',
+ *       '5 cubed = ' + module.cube(5)
+ *     ].join('\n\n');
+ * 
+ *     return elm;
+ *   }
+ * 
+ *   document.body.appendChild(component());
+ * });
+ */
 
-  return elm;
-}
+// function component() {
+//   const elm = document.createElement('pre');
+//   elm.innerHTML = [
+//     'Hello Webpack',
+//     '5 cubed = ' + cube(5)
+//   ].join('\n\n');
 
-document.body.appendChild(component());
+//   return elm;
+// }
+
+map();
+
+// document.body.appendChild(component());
